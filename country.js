269bb5046aa3a7e1fd64b67efbbf2414e1fb6488
radeen <radeen013@gmail.com>
2021-11-10 09:17:24 -0500

    come back to dress this up later

--- country.js
+++ country.js
@@ -5,14 +5,26 @@ class Country {
         this.greeting = greeting;
         this.colors = colors;
     }
+    Display(){
+        document.getElementById("CountryName").innerText = this.name;
+        document.getElementById("OfficialLanguage").innerText = this.lang;
+        document.getElementById("HelloWorld").innerText = this.greeting;
+        
+        document.getElementById("Color1").style.backgroundColor = this.colors[0];
+        document.getElementById("Color2").style.backgroundColor = this.colors[1];
+        document.getElementById("Color3").style.backgroundColor = this.colors[2];
+    }
 }
 let usa = new Country("USA", "Murican", "Why Hello there world! Have some McDonalds", ["red", "white", "blue"]);
 let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"]);
-let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
+let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"]);
+let japan = new Country("Japan", "Japanese", "Konichiwa", ["red", "white"] );
+let bangladesh = new Country("Bangladesh", "Bengali", "Balo achen", ["red", "green"])
 
 
 function SwitchCountry() {
-    let input = "USA";
+    let input = document.getElementById("CountryList").value;
+    console.log(input)
     let country;
 
     if (input === "USA") {
@@ -25,5 +37,13 @@ function SwitchCountry() {
     else if (input === "Algeria") {
         country = algeria;
     }
+    else if (input === "Japan") {
+        country = japan;
+    }
+    else if (input === "Bangladesh") {
+        country = bangladesh;
+    }
+
+    country.Display();
 
 }
